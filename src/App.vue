5acd6f32cb3123c95046986a244214f682e52a21
Gloomy-poplar <egor061297@gmail.com>
2019-01-13 23:54:32 +0300

    First

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <router-view/>
+  </div>
+</template>
+<script>
+  import AppHeader from '@/components/AppHeader';
+  import autor from '@/components/autor';
+
+  export default {
+    components: {
+      AppHeader,
+      autor
+    }
+  }
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app>
+    <v-toolbar class='orange lighten-4'>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" class="white--text btn">
+        Login
+        <v-icon>home</v-icon>
+      </v-btn>
+    </v-toolbar>
+<v-form v-model="valid">
+    <v-container>
+      <v-layout>
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex
+          xs12
+          md4
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-layout align-center justify-end row><v-btn color="success">accept</v-btn></v-layout>
+  
+ 
+ 
+ 
+ 
+ </v-form>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style styles>
+
+
+</style>
